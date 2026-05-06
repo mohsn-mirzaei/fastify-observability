1172ee72674a4190c1414ab7fbdd7f6f171d51a9
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2026-05-06 13:32:34 +0330

    add simple log with pino & use pino-pretty for better DX

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,57 @@
 import Fastify from "fastify";
 
 const fastify = Fastify({
-  logger: true,
+  logger: {
+    level: "info", // trace, debug, info, warn, error
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
 });
 
-fastify.get("/", async () => {
-  return { hello: "world" };
+// endpoint info log
+fastify.get("/info-log", async (request, reply) => {
+  request.log.info("this is info log");
+  return { message: "Hello World" };
+});
+
+// endpoint debug-log
+fastify.get("/debug-log/:id", async (request, reply) => {
+  const { id } = request.params as { id: string };
+  request.log.debug(`this is debug log: ${id}`);
+
+  return { message: "Hello World" };
+});
+
+// endpoint error-log
+fastify.get("/error-log", async (request, reply) => {
+  request.log.error("this is error log");
+
+  return { message: "Hello World" };
+});
+
+// endpoint warn-log
+fastify.get("/warn-log", async (request, reply) => {
+  request.log.warn("this is warn log");
+
+  return { message: "Hello World" };
+});
+
+// endpoint warn-log
+fastify.get("/all-log", async (request, reply) => {
+  request.log.info("this is info log");
+  request.log.debug("this is debug log");
+  request.log.error("this is error log");
+  request.log.fatal("this is fatal log");
+  request.log.trace("this is trace log");
+  request.log.silent("this is silent log");
+  request.log.level;
+
+  return { message: "Hello World" };
 });
 
 const start = async () => {
